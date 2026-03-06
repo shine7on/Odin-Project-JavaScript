5b701fa81b13d3790913ea6ed91524d8e18954c0
shine7on <pabboo13@gmail.com>
2026-03-05 18:21:34 -0600

    Finish sumAll exercise [Number.isInteger()]

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(param1, param2) {
+    let start = param1;
+    let end = param2;
 
+    if (param1 > param2) {
+        start = param2;
+        end = param1;
+    }
+
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR';
+    }
+
+    let sum = 0;
+
+    for (let i = start; i < end + 1; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
